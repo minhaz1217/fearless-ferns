eab00bcff2127f121a3bf85b67680673a95f0d69
SaadZahem <saadzahem7@gmail.com>
2025-08-17 02:15:08 +0300

    chore: created a new directory for custom components and reimplemented CustomEditor using

--- src/project/editor.py
+++ src/project/editor.py
@@ -1,49 +1,18 @@
 from __future__ import annotations
 
 import operator as op
-from typing import TYPE_CHECKING
 
 from html2text import html2text
 from nicegui import app, ui
 from nicegui.elements.mixins.disableable_element import DisableableElement
 from nicegui.elements.mixins.value_element import ValueElement
 
-if TYPE_CHECKING:
-    from nicegui.events import Handler, ValueChangeEventArguments
 
-
-# based on `ui.editor` and these examples https://quasar.dev/vue-components/editor
-class CustomEditor(
-    ValueElement,
-    DisableableElement,
-    component="editor.vue",
-    default_classes="nicegui-editor",
-):
+class CustomEditor(ui.editor, component="components/editor.vue"):
     """Custom editor."""
 
-    VALUE_PROP: str = "value"
-    LOOPBACK = False
-
-    def __init__(
-        self,
-        *,
-        placeholder: str | None = None,
-        value: str = "",
-        on_change: Handler[ValueChangeEventArguments] | None = None,
-    ) -> None:
-        super().__init__(value=value, on_value_change=on_change)
-        if placeholder is not None:
-            self._props["placeholder"] = placeholder
-
-    def _handle_value_change(self, value) -> None:
-        super()._handle_value_change(value)
-        if self._send_update_on_value_change:
-            self.run_method("updateValue")
-
     def apply_styles(self):
-        return self.classes("size-full flex flex-col items-stretch").props(
-            'content-class="grow"',
-        )
+        return self.classes("size-full flex flex-nowrap flex-col items-stretch").props('content-class="grow"')
 
 
 async def index() -> None:
