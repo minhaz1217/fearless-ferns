94077ccb30251295aa212caea65b3a08c9d68df3
SaadZahem <saadzahem7@gmail.com>
2025-08-18 19:03:07 +0300

    fix: disabled homepage. It now redirects to editor page

--- src/project/pages/homepage.py
+++ src/project/pages/homepage.py
@@ -3,9 +3,14 @@ from __future__ import annotations
 from nicegui import ui
 
 
-def index():
-    with ui.element().classes("size-full flex place-content-center"):
-        with ui.card().classes("container flex items-center"):
-            ui.label("Try our crazy chart editor").classes("text-h4 font-bold font-serif")
-            with ui.link(target="/editor"):
-                ui.button("Try it now!").props("rounded").classes("px-8 py-2")
+def _index() -> None:
+    """Disabled homepage."""
+    with ui.element().classes("size-full flex place-content-center"), ui.card().classes("container flex items-center"):
+        ui.label("Try our crazy chart editor").classes("text-h4 font-bold font-serif")
+        with ui.link(target="/editor"):
+            ui.button("Try it now!").props("rounded").classes("px-8 py-2")
+
+
+def index() -> None:
+    """Active code for homepage. Redirect user to editor page."""
+    ui.navigate.to("/editor")
